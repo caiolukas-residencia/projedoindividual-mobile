6a808b12999dcb438dffaf4f30acedeab76fcf38
caiolukas-residencia <caio.rocha@residente.serratec.org>
2026-06-17 04:20:45 -0300

    Criação dos arquivos base

---
+++ replica-slowly/src/theme/colors.tsx
@@ -0,0 +1,10 @@
+export const Colors = {
+  background: '#2A2F3D',
+  card: '#363C4D',
+  textPrimary: '#FFFFFF',
+  textSecondary: '#A0A5B5',
+  accentYellow: '#F4C744',
+  buttonWhite: '#FFFFFF',
+  textDark: '#1A1D26',
+  border: '#4A4F5C',
+};
